3f018f55799f4f07634d434e924301703df5f4b0
Viet Nguyen <vietnhprivat@gmail.com>
2026-01-09 19:43:12 +0100

    Uncommented unfinished code in train.py and added to do

--- src/stuperml/train.py
+++ src/stuperml/train.py
@@ -1,9 +1,11 @@
 from stuperml.model import Model
 from stuperml.data import MyDataset
 
+# TODO This function still needs to be implemented. 
 def train():
-    dataset = MyDataset("data/raw")
-    model = Model()
+    # dataset = MyDataset("data/raw")
+    # model = Model()
+    pass
     # add rest of your training code here
 
 if __name__ == "__main__":
