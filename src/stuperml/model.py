4671b5fa4147b0fcddc2c428816382cad17841ba
Viet Nguyen <vietnhprivat@gmail.com>
2026-01-09 13:35:46 +0100

    Defined simple initial 3 layer model class

--- src/stuperml/model.py
+++ src/stuperml/model.py
@@ -1,16 +1,23 @@
-from torch import nn
 import torch
+import torch.nn as nn
 
 class Model(nn.Module):
-    """Just a dummy model to show how to structure your code"""
-    def __init__(self):
+    def __init__(self, input_size: int):
         super().__init__()
-        self.layer = nn.Linear(1, 1)
+        self.fc1 = nn.Linear(input_size, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 1)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.layer(x)
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.sigmoid(self.fc3(x))
+        return x * 100
 
 if __name__ == "__main__":
-    model = Model()
     x = torch.rand(1)
+    model = Model(input_size=x.shape[0])
     print(f"Output shape of model: {model(x).shape}")
+    
