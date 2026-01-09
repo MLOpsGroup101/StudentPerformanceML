daacb434f40b25cd693811eec4df36c30eb35225
MHH1602 <mads@hojgaard.eu>
2026-01-09 10:42:38 +0100

    Creating initial project structure

---
+++ StudentPerfomanceML/src/stuperml/train.py
@@ -0,0 +1,10 @@
+from stuperml.model import Model
+from stuperml.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
