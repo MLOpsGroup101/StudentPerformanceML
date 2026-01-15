2980997433c964d96bfcdc0c777e269a01f426a7
Viet Nguyen <vietnhprivat@gmail.com>
2026-01-15 14:39:16 +0100

    feat: Added test with aprox 80% coverage

--- configs/src/configs/__init__.py
+++ configs/src/configs/__init__.py
@@ -1,15 +1,3 @@
-# from pathlib import Path
-
-# data_cfg = dict(
-#     data_folder=Path("data/"),
-#     dropped_columns=[],
-#     target_col="final_score",
-#     train_size=0.8,
-#     test_size=0.1,
-#     val_size=0.1,
-# )
-
-
 from dataclasses import dataclass, field
 from pathlib import Path
 
@@ -17,6 +5,7 @@ from pathlib import Path
 @dataclass(frozen=True)
 class DataConfig:
     data_folder: Path = Path("data/")
+    file_names: tuple[str, ...] = ("X_train.pt", "X_val.pt", "X_test.pt", "y_train.pt", "y_val.pt", "y_test.pt")
     target_col: str = "final_score"
     dropped_columns: list[str] = field(default_factory=list)
     train_size: float = 0.8
@@ -24,5 +13,6 @@ class DataConfig:
     val_size: float = 0.1
     seed: int = 42
 
-
 data_config = DataConfig()
+
+
